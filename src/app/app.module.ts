d6b67ea4167ffe3e12ca0d9ce7608b1bcae4d425
JMarioBorrayo <jumagonzalezbo@ittepic.edu.mx>
2022-11-09 01:01:52 -0700

    navbar - Carousel
    Aun no funciona el carrusel de imagenes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Total Salud/home/home.component';
+import { NavbarComponent } from './Total Salud/navbar/navbar.component';
+import { CarruselComponent } from './Total Salud/carrusel/carrusel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    CarruselComponent
   ],
   imports: [
     BrowserModule,
